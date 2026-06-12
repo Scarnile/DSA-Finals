668fa4acea5432178079d3ee233d3273ee53541d
Scarnile <198330506+Scarnile@users.noreply.github.com>
2026-06-12 12:08:11 +0800

    Add basic sidebar and top layout

--- src/main/java/com/example/dsafinals/Launcher.java
+++ src/main/java/com/example/dsafinals/Launcher.java
@@ -5,5 +5,5 @@ import javafx.application.Application;
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(MainApplication.class, args);
-    }
+    }   
 }
--- src/main/java/com/example/dsafinals/MainApplication.java
+++ src/main/java/com/example/dsafinals/MainApplication.java
@@ -11,7 +11,8 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1220, 640);
+        stage.setResizable(true);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
