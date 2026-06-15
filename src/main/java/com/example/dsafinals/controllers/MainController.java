06f226319de6b5878aab9fe36961a263918f6d32
Scarnile <198330506+Scarnile@users.noreply.github.com>
2026-06-15 13:28:22 +0800

    Make content area display on button click

--- src/main/java/com/example/dsafinals/controllers/MainController.java
+++ src/main/java/com/example/dsafinals/controllers/MainController.java
@@ -1,9 +1,15 @@
 package com.example.dsafinals.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import org.kordamp.ikonli.javafx.FontIcon;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainController {
     @FXML
@@ -27,10 +33,11 @@ public class MainController {
     @FXML
     private Button newEntryButton;
 
+    @FXML
+    private StackPane contentArea;
 
     @FXML
     public void initialize() {
-
         dashboardButton.setGraphic(createIcon("mdi2h-home"));
         journalButton.setGraphic(createIcon("mdi2b-book-open-page-variant"));
         albumsButton.setGraphic(createIcon("mdi2f-folder"));
@@ -38,12 +45,35 @@ public class MainController {
         settingsButton.setGraphic(createIcon("mdi2c-cog"));
         newEntryButton.setGraphic(createIcon("mdi2p-plus"));
 
+
         // Resize the sidebar based on the window width
         sidebar.sceneProperty().addListener((obs, oldScene, newScene) -> {
             if (newScene != null) {
                 sidebar.prefWidthProperty().bind(newScene.widthProperty().multiply(0.17));
             }
         });
+
+
+//        URL url = getClass().getResource("/com/example/dsafinals/fxml/dashboard.fxml");
+//        System.out.println(url);
+//        loadPage("/fxml/dashboard.fxml");
+        dashboardButton.setOnAction(e -> {
+            loadPage("/fxml/dashboard.fxml");
+        });
+    }
+
+    private void loadPage(String fxml) {
+        try {
+            URL url = getClass().getResource("/com/example/dsafinals/fxml/dashboard.fxml");
+            System.out.println(url);
+
+            Parent page = FXMLLoader.load(url);
+
+            contentArea.getChildren().setAll(page);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public FontIcon createIcon(String iconCode) {
