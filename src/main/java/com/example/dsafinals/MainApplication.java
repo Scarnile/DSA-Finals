9736373575bf604b6b7e77a00f7e1c82e19255e0
Scarnile <198330506+Scarnile@users.noreply.github.com>
2026-06-17 18:07:54 +0800

    Remove titlebar and make the whole window movable

--- src/main/java/com/example/dsafinals/MainApplication.java
+++ src/main/java/com/example/dsafinals/MainApplication.java
@@ -2,13 +2,20 @@ package com.example.dsafinals;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class MainApplication extends Application {
+
+    // Store offsets as class fields
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("fxml/main.fxml"));
@@ -20,6 +27,20 @@ public class MainApplication extends Application {
         // Set the minimum bounds for the window
         stage.setMinWidth(800);
         stage.setMinHeight(550);
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        // Remove titlebar and make the whole window movable
+        Parent root = stage.getScene().getRoot();
+
+        root.setOnMousePressed(event -> {
+            xOffset = stage.getX() - event.getScreenX();
+            yOffset = stage.getY() - event.getScreenY();
+        });
+
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() + xOffset);
+            stage.setY(event.getScreenY() + yOffset);
+        });
 
         // CSS
         String css = Objects.requireNonNull(getClass().getResource("stylesheets/application.css")).toExternalForm();
