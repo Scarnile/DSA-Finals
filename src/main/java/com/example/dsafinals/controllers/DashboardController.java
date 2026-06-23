8391d9eef41c99c2e7abc3e54e8c788663255ff2
Scarnile <198330506+Scarnile@users.noreply.github.com>
2026-06-23 13:40:02 +0800

    Change root of dashboard to be a VBox instead of a ScrollPane

--- src/main/java/com/example/dsafinals/controllers/DashboardController.java
+++ src/main/java/com/example/dsafinals/controllers/DashboardController.java
@@ -1,21 +1,4 @@
 package com.example.dsafinals.controllers;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.ScrollPane;
-
 public class DashboardController {
-    @FXML
-    private ScrollPane scrollPane;
-
-    @FXML
-    public void initialize() {
-        // you can safely use it here
-        scrollPane.setPannable(true);
-        scrollPane.setFocusTraversable(false);
-        scrollPane.setOnMouseClicked(e -> scrollPane.requestFocus());
-
-        scrollPane.setPannable(true);
-        scrollPane.setFitToWidth(true);
-        scrollPane.setFitToHeight(true);
-    }
 }
